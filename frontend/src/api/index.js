189daf2ef5aa853cd93c824dc2597d948b2d38b5
Nameless_Eternal <176422595+CalamitousLightning@users.noreply.github.com>
2026-08-18 14:18:49 +0100

    Update index.js

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || " https://evosgpt-rebirth.fly.dev",
+  baseURL: import.meta.env.VITE_API_URL || "https://evosgpt-rebirth.fly.dev",
 });
 
 export const registerUser = (d)  => API.post("/auth/register", d);
